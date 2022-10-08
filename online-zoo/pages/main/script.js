35594d3c67064401f0874560f729ae16abc3c20f
mshns <mishanos412@gmail.com>
2022-10-08 20:53:19 +0300

    feat: add slider for desktop, small desktop and tablet

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,3 +1,92 @@
+// slider
+
+const buttonLeft = document.querySelector(".button-arrow-left");
+const buttonRight = document.querySelector(".button-arrow-right");
+const slider = document.querySelector(".slider");
+
+const leftSlide = document.querySelector(".slider-left");
+const centerSlide = document.querySelector(".slider-center");
+const rightSlide = document.querySelector(".slider-right");
+
+
+const moveLeft = () => {
+  slider.classList.add("transition-left");
+};
+
+const moveRight = () => {
+  slider.classList.add("transition-right");
+};
+
+buttonLeft.addEventListener("click", moveLeft);
+buttonRight.addEventListener("click", moveRight);
+
+
+slider.addEventListener("animationend", function(animationEvent) {
+
+    const leftContent = document.querySelector(".slider-left").innerHTML;
+    const centerContent = document.querySelector(".slider-center").innerHTML;
+    const rightContent = document.querySelector(".slider-right").innerHTML;
+
+    if (animationEvent.animationName === "move-left") {
+      slider.classList.remove("transition-left");
+      leftSlide.innerHTML = rightContent;
+
+      centerSlide.innerHTML = leftContent;
+      rightSlide.innerHTML = centerContent;
+      rightSlide.firstElementChild.style.order = Math.round(Math.random()*5 + 1);
+      rightSlide.lastElementChild.style.order = Math.round(Math.random()*5 - 1);
+    } else {
+      slider.classList.remove("transition-right");
+      leftSlide.innerHTML = centerContent;
+      leftSlide.firstElementChild.style.order = Math.round(Math.random()*5 + 1);
+      leftSlide.lastElementChild.style.order = Math.round(Math.random()*5 - 1);
+      centerSlide.innerHTML = rightContent;
+      rightSlide.innerHTML = leftContent;
+    }
+  })
+
+// burger menu
+
+const burgerButton = document.querySelector(".burger-menu-icon");
+const burgerMenu = document.querySelector(".burger-menu");
+const burgerIcon = document.querySelector(".burger-menu-icon");
+const shadow = document.querySelector(".shadow");
+const menuLink = document.querySelectorAll(".burger-menu-link");
+const menuCross = document.querySelector(".burger-menu-cross")
+
+burgerButton.addEventListener('click',
+  function() {
+    burgerMenu.classList.toggle("active");
+    shadow.classList.toggle("active");
+  }
+);
+
+menuCross.addEventListener('click',
+  function() {
+    burgerMenu.classList.toggle("active");
+    shadow.classList.toggle("active");
+  }
+);
+
+shadow.addEventListener('click',
+  function() {
+    burgerMenu.classList.toggle("active");
+    shadow.classList.toggle("active");
+  }
+);
+
+menuLink.forEach((el) => {
+    el.addEventListener('click',
+      function() {
+        burgerMenu.classList.toggle("active");
+        shadow.classList.toggle("active");
+      }
+    )
+  }
+);
+
+// score
+
 console.log(
   'Contacts:\n',
   '📧 Discord: mishanos#6940\nhttps://discordapp.com/users/561035807046238209\n',
